77e24f8fd3b82cace97e4cf809385a64306717dc
Olesya Miller <olesyam.miller@gmail.com>
2019-12-11 02:31:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
 def oxford_comma(array)
   
+  if array.length == 1 
+    return array.join 
+  
   if array.length == 2 
     return array.join(" and ")  
   end 
+  
   if array.length  > 2 
-    return array[0..-2].join(", ") + ", and " + array[-1].to_s 
+    return array[0..-2].join(", ") + ", and " + array[-1].to_s
   end 
 end
