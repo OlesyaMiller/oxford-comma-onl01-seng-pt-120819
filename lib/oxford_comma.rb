db11c02e4fddff091d5713c5a961fc17c996bb53
Olesya Miller <olesyam.miller@gmail.com>
2019-12-11 02:26:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
+  
   if array.size == 2 
-    array.join(" , ")  
+    array.join(" and ")  
   end 
   if array.size  > 2 
     array[0..-2].join(" , ") + "and" + array[-1].to_s 
